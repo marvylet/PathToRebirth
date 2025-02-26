01f8612af3d1116e392d17d7628b8a5d60af91cc
Marv <marvylet6@volny.cz>
2025-02-26 11:56:57 +0100

    Updated locations file, added file for character IDs, added a class for figuring out locations and added a constructor for MansionMap

--- src/MansionMap.java
+++ src/MansionMap.java
@@ -1,3 +1,13 @@
-public class MansionMap {
+import java.util.HashMap;
 
+public class MansionMap {
+    private HashMap<Integer, Location> locations;
+    private int startingLoc;
+    private int currentLoc;
+
+    public MansionMap() {
+        this.locations = new HashMap<>();
+        this.startingLoc = 1;
+        this.currentLoc = startingLoc;
+    }
 }
