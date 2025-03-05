a33cf2a0a38de0a31bb230581da2341b2b898741
Marv <marvylet6@volny.cz>
2025-03-05 10:26:43 +0100

    Added parameters, constructor, getters and setters to class Location.

--- src/Location.java
+++ src/Location.java
@@ -1,5 +1,46 @@
+import java.util.ArrayList;
+
 public class Location {
+    private int ID;
+    private String name;
+    private ArrayList<Integer> connectedRooms;
 
+    public Location(int ID, String name, ArrayList<Integer> connectedRooms) {
+        this.ID = ID;
+        this.name = name;
+        this.connectedRooms = connectedRooms;
+    }
 
+    public int getID() {
+        return ID;
+    }
 
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Integer> getConnectedRooms() {
+        return connectedRooms;
+    }
+
+    public void setConnectedRooms(ArrayList<Integer> connectedRooms) {
+        this.connectedRooms = connectedRooms;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", connectedRooms=" + connectedRooms +
+                '}';
+    }
 }
