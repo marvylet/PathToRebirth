5070abe746bf0c68e568df84217ae796a286fc0e
Marv <marvylet6@volny.cz>
2025-03-11 21:59:38 +0100

    Added inicialize, doCommand and start methods, wrote inicialize one

--- src/Console.java
+++ src/Console.java
@@ -1,2 +1,27 @@
+import Commands.*;
+
+import java.util.HashMap;
+
 public class Console {
+    private boolean exit = false;
+    private HashMap<String, Command> commands;
+
+    private void inicialize(){
+        commands = new HashMap<>();
+
+        commands.put("go", new GoTo());
+        commands.put("grab", new Grab());
+        commands.put("interact", new Interact());
+        commands.put("move", new MoveItem());
+        commands.put("diary", new OpenDiary());
+        commands.put("inventory", new OpenInventory());
+        commands.put("talk", new TalkTo());
+        commands.put("use", new Use());
+    }
+
+    private void doCommand(){
+
+    }
+
+    private void start(){}
 }
