eebc6f85324be4b6221f46f7b837c923b20e1b06
Marv <marvylet6@volny.cz>
2025-03-12 11:44:38 +0100

    Wrote Grab command

--- src/Commands/Grab.java
+++ src/Commands/Grab.java
@@ -1,9 +1,22 @@
 package Commands;
 
+import Items.Item;
+import Map.Inventory;
+
 public class Grab extends Command{
+    private Inventory inv;
+    private Item item;
+
     @Override
     public String execute() {
-        return "";
+
+        if(item.isGrabbable()){
+            inv.addItem(item.getName(), item);
+            return "Item successfully grabbed and added in inventory.";
+        }else{
+            return "Item is not grabbable.";
+        }
+
     }
 
     @Override
