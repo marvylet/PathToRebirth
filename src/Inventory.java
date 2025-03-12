91c9fd54093a483b05ba24d9b8ce9ac495940ab9
Marv <marvylet6@volny.cz>
2025-03-12 11:40:50 +0100

    Added Class Inventory and added method to add items in it

---
+++ src/Inventory.java
@@ -0,0 +1,22 @@
+import Items.Item;
+
+import java.util.HashMap;
+
+public class Inventory {
+    private int capacity;
+    private HashMap<String, Item> items;
+
+    public Inventory() {
+        capacity = 3;
+        items = new HashMap<>();
+    }
+
+    public boolean addItem(String name, Item item){
+        if(items.size() < capacity){
+            items.put(name, item);
+            return true;
+        }
+        return false;
+    }
+
+}
