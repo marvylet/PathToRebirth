d2f83f04382c6309e57108555b61fb5558934dbc
Marv <marvylet6@volny.cz>
2025-03-12 10:53:35 +0100

    Wrote CloseGame command methods

--- src/Commands/CloseGame.java
+++ src/Commands/CloseGame.java
@@ -1,13 +1,27 @@
 package Commands;
 
+import java.util.Scanner;
+
 public class CloseGame extends Command {
+    private boolean exit;
+
     @Override
     public String execute() {
-        return "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Are you sure you want to exit?");
+        String answer = sc.next();
+
+        if(answer.equalsIgnoreCase("yes")){
+            exit = true;
+            return "Closing program...";
+        }else{
+            exit = false;
+            return "Failed to answer.";
+        }
     }
 
     @Override
     public boolean exit() {
-        return false;
+        return exit;
     }
 }
