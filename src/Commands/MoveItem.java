e9396ce03692ca60821689b5bd0c72ea7635299a
Marv <marvylet6@volny.cz>
2025-03-18 20:27:36 +0100

    Wrote MoveItem command

--- src/Commands/MoveItem.java
+++ src/Commands/MoveItem.java
@@ -1,9 +1,15 @@
 package Commands;
 
+import Items.Item;
+
 public class MoveItem extends Command {
+    private Item item;
     @Override
     public String execute() {
-        return "";
+        if(item.moveable()){
+            return "Item successfully moved.";
+        }
+        return "Not able to move the item.";
     }
 
     @Override
