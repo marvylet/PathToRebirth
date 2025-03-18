7e7912a4a24d906675ed4f053cfaa53b38f24adf
Marv <marvylet6@volny.cz>
2025-03-18 19:08:21 +0100

    Added methods for Inventory

--- src/Map/Inventory.java
+++ src/Map/Inventory.java
@@ -2,23 +2,42 @@ package Map;
 
 import Items.Item;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Inventory {
     private int capacity;
     private HashMap<String, Item> items;
+    private ArrayList<String> itemNames;
 
     public Inventory() {
         capacity = 5;
         items = new HashMap<>();
+        itemNames = new ArrayList<>();
     }
 
     public boolean addItem(String name, Item item){
         if(items.size() < capacity){
             items.put(name, item);
+            itemNames.add(name);
             return true;
         }
         return false;
+    }
+
+    public String viewInventory(){
+        String inventory = "";
+        for(int i = 0; i < itemNames.size(); i++){
+            inventory += "  " + itemNames.get(i) + "\n";
+        }
+        return inventory;
+    }
+
+    public String viewItemDesc(String name){
+        if(items.containsKey(name)){
+            return items.get(name).getDescription();
+        }
+        return "No such item";
     }
 
 }
