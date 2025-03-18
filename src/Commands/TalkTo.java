4c81b0d97804a0e70d9d57c3aafb7331e20efb35
Marv <marvylet6@volny.cz>
2025-03-18 21:40:42 +0100

    Wrote TalkTo command

--- src/Commands/TalkTo.java
+++ src/Commands/TalkTo.java
@@ -2,13 +2,24 @@ package Commands;
 
 import Map.MansionMap;
 
+import java.util.Scanner;
+
 public class TalkTo extends Command {
+    private Scanner sc;
     private MansionMap map;
     private Character character;
     @Override
     public String execute() {
+        sc = new Scanner(System.in);
 
-        return "";
+        System.out.println("You can talk to:");
+        System.out.println(map.getLocations().get(map.getCurrentLoc()).viewChararacters());
+        String s = sc.next();
+
+        if(map.getLocations().get(map.getCurrentLoc()).getCharacters().contains(character)) {
+            return map.getLocations().get(map.getCurrentLoc()).getCharacters().get(Integer.parseInt(s)).getDialogue();
+        }
+        return "No such character";
     }
 
     @Override
