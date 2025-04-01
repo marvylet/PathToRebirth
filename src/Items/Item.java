ab1b7ee4a5467eaab55c4f8dc864751bd6683e6f
Marv <marvylet6@volny.cz>
2025-04-01 22:47:06 +0200

    Added JavaDocs to Item

--- src/Items/Item.java
+++ src/Items/Item.java
@@ -3,6 +3,9 @@ package Items;
 
 import java.util.ArrayList;
 
+/**
+ * An abstract class used for creating items
+ */
 public abstract class Item {
     private String name;
     private ArrayList<Integer> rooms;
@@ -20,17 +23,40 @@ public abstract class Item {
         this.name = name;
     }
 
-
+    /**
+     * Abstract method used for getting the description of the item
+     * @return - returns String value of item's description
+     */
     public abstract String getDescription();
 
+    /**
+     * Abstract method used for getting an effect if it has one
+     * @return - returns String value of an effect if the item has one
+     */
     public abstract String getEffect();
 
+    /**
+     * Abstract method which tells if the item has an effect or not
+     * @return - true if has an effect, otherwise false
+     */
     public abstract boolean effect();
 
+    /**
+     * Abstract method which tells if the item is grabbable or not
+     * @return - true if grabbable, otherwise false
+     */
     public abstract boolean grabbable();
-
+    /**
+     * Abstract method which tells if the item is moveable or not
+     * @return - true if moveable, otherwise false
+     */
     public abstract boolean moveable();
 
+    /**
+     * A factory method used in MansionMap when adding items from a file to a room
+     * @param itemName - name of the item which is being added
+     * @return - returns a switch which creates an item depending on itemName
+     */
     public static Item factory(String itemName){
         return switch(itemName){
             case "Book" -> new Book(itemName);
